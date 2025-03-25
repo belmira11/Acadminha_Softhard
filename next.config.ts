ad2a3bebc0528b25f3511656fe2601e88fffb1f1
devbelmira11 <belmirarodrigues15@gmail.com>
2025-03-25 13:29:59 -0300

    Salvando mudanças locais antes do pull

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true, // Ativa o modo estrito do React
   trailingSlash: false,  // Define se as URLs devem terminar com "/"
+
+  output: 'export',
+  images: {
+    unoptimized: true, // GitHub Pages não suporta otimização de imagens do Next.js
+  },
+
 };
 
 module.exports = nextConfig;
